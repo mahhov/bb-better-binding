9d87e6f4fc8f9b9110668fe35d002ea835f0d05e
m <mahhov1@gmail.com>
2018-04-26 15:45:03 -0700

    inherited __bindIgnore__

--- src/source.js
+++ src/source.js
@@ -6,21 +6,24 @@ let createSource = () => {
 
 let ignore = [];
 
+let isBindIgnored = (obj, prop) => obj.__bindIgnore__ && obj.__bindIgnore__.includes(prop);
+
 let createProxy = (obj, handlers, accumulatedHandlers = []) => new Proxy(obj, {
     get: (target, prop) => {
         let got = Reflect.get(target, prop);
-        return typeof got === 'object' && got !== null ? createProxy(got, handlers && handlers[prop], accumulatedHandlers.concat(handlers)) : got;
+        return typeof got === 'object' && got !== null && !isBindIgnored(obj, prop) ? createProxy(got, handlers && handlers[prop], accumulatedHandlers.concat(handlers)) : got;
     },
     set: (target, prop, value) => {
         Reflect.set(target, prop, value);
 
-        if (obj.__bindIgnore__ && obj.__bindIgnore__.includes(prop))
+        if (isBindIgnored(obj, prop))
             return true;
 
-        if (obj.__bindAvoidCycles__ && ignore.some(ignore => ignore.target === target && ignore.prop === prop))
+        // todo make __bindAvoidCycles__ inherited and maybe avoid per binding instead per change
+        if (obj.__bindAvoidCycles__ && ignore.some(ignore => ignore.obj === obj && ignore.prop === prop))
             return true;
 
-        ignore.push({target, prop, value});
+        ignore.push({prop, obj});
         accumulatedHandlers.forEach(doHandler);
         doHandler(handlers);
         handlers && propogateHandlerDown(handlers[prop]);
