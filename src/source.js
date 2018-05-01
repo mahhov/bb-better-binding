50fa257c49e8792a84068face2f886eef221c64a
m <mahhov1@gmail.com>
2018-04-30 18:59:26 -0700

    wip source to watch original objects

--- src/source.js
+++ src/source.js
@@ -8,27 +8,48 @@ let ignore = [];
 
 let isBindIgnored = (obj, prop) => obj.__bindIgnore__ && obj.__bindIgnore__.includes(prop);
 
+let proxyGet = (target, prop, obj, handlers, accumulatedHandlers) => {
+    let got = Reflect.get(target, prop);
+    return typeof got === 'object' && got !== null && !isBindIgnored(obj, prop) ? createProxy(got, handlers && handlers[prop], accumulatedHandlers.concat(handlers)) : got;
+};
+
+let proxySet = (target, prop, value, obj, handlers, accumulatedHandlers) => {
+    if (isBindIgnored(obj, prop))
+        return true;
+
+    // todo make __bindAvoidCycles__ inherited and maybe avoid per binding instead per change
+    if (obj.__bindAvoidCycles__ && ignore.some(ignore => ignore.obj === obj && ignore.prop === prop))
+        return true;
+
+    ignore.push({prop, obj});
+    accumulatedHandlers.forEach(doHandler);
+    doHandler(handlers);
+    handlers && propogateHandlerDown(handlers[prop]);
+    ignore.pop();
+};
+
 let createProxy = (obj, handlers, accumulatedHandlers = []) => new Proxy(obj, {
     get: (target, prop) => {
-        let got = Reflect.get(target, prop);
-        return typeof got === 'object' && got !== null && !isBindIgnored(obj, prop) ? createProxy(got, handlers && handlers[prop], accumulatedHandlers.concat(handlers)) : got;
+        return proxyGet(target, prop, obj, handlers, accumulatedHandlers);
     },
     set: (target, prop, value) => {
-        Reflect.set(target, prop, value);
+        if (value && typeof value === 'object' && handlers && handlers[prop])
+            Object.entries(handlers[prop]).forEach(([key, handlerValue]) => {
+                if (key !== '_func_') {
+                    let watchKey = `_${key}_`;
+                    value[watchKey] = value[key];
+                    watch(value, key, () => {
+                        proxyGet(target, prop, obj, handlers, accumulatedHandlers);
+                        return value[watchKey];
+                    }, newValue => {
+                        value[watchKey] = newValue;
+                        proxySet(target, prop, value, obj, handlers, accumulatedHandlers)
+                    });
+                }
+            });
 
-        if (isBindIgnored(obj, prop))
-            return true;
-
-        // todo make __bindAvoidCycles__ inherited and maybe avoid per binding instead per change
-        if (obj.__bindAvoidCycles__ && ignore.some(ignore => ignore.obj === obj && ignore.prop === prop))
-            return true;
-
-        ignore.push({prop, obj});
-        accumulatedHandlers.forEach(doHandler);
-        doHandler(handlers);
-        handlers && propogateHandlerDown(handlers[prop]);
-        ignore.pop();
-
+        Reflect.set(target, prop, value); // move this?
+        proxySet(target, prop, value, obj, handlers, accumulatedHandlers);
         return true;
     }
 });
@@ -43,6 +64,18 @@ let propogateHandlerDown = handlers => {
 let doHandler = handler => {
     if (handler && typeof handler._func_ === 'function')
         handler._func_();
+};
+
+let watchAll = () => {
+    // todo bring logic from set to here
+};
+
+let watch = (obj, key, getHandler, setHandler) => {
+    Object.defineProperty(obj, key, {
+        get: () => getHandler(),
+        set: newValue => setHandler(newValue),
+        configurable: true
+    });
 };
 
 module.exports = {createSource};
