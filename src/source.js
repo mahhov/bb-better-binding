727d7d43aca7232c2646affa57c7fef0b15adfb6
m <mahhov1@gmail.com>
2018-05-02 09:02:06 -0700

    clean up source

--- src/source.js
+++ src/source.js
@@ -13,26 +13,22 @@ let createSourceWithHandlers = handlers => {
     return source;
 };
 
-let proxyGet = (got, handlers, accumulatedHandlers) => {
+let getHandler = (got, handlers, accumulatedHandlers) => {
     return typeof got === 'object' && got ? createProxy(got, handlers, accumulatedHandlers) : got;
 };
 
-let proxySet = (prop, handlers, accumulatedHandlers) => {
-    console.log('>>>> handler set', prop, handlers, accumulatedHandlers);
+let setHandler = (prop, handlers, accumulatedHandlers) => {
     accumulatedHandlers.forEach(doHandler);
-    handlers && propogateHandlerDown(handlers);
+    handlers && propogateHandlerDown(handlers); // todo which handler null checks r necessary
 };
 
 let createProxy = (obj, handlers, accumulatedHandlers = []) => new Proxy(obj, {
-    get: (target, prop) => {
-        return Reflect.get(target, prop);
-    },
+    get: (target, prop) =>
+        Reflect.get(target, prop),
+
     set: (target, prop, value) => {
-        if (value && typeof value === 'object' && handlers && handlers[prop]) {
-            let specialHandler = handlers[prop];
-            let specialAccumulatedHandlers = accumulatedHandlers.concat(handlers);
-            watchAll(value, specialHandler, specialAccumulatedHandlers);
-        }
+        if (value && typeof value === 'object' && handlers && handlers[prop])
+            watchAll(value, handlers[prop], accumulatedHandlers.concat(handlers));
 
         Reflect.set(target, prop, value);
         return true;
@@ -42,15 +38,9 @@ let createProxy = (obj, handlers, accumulatedHandlers = []) => new Proxy(obj, {
 let propogateHandlerDown = handlers => {
     doHandler(handlers);
 
-    // Object.entries(handlers);
-    //     .filter(([key]) => key !== '_func_')
-    //     .forEach(([key, handlerValue]) => {
-    //     });
-    // todo is this better than `for in`?
-
-    for (key in handlers)
-        if (key !== '_func_')
-            propogateHandlerDown(handlers[key]);
+    Object.entries(handlers)
+        .filter(([key]) => key !== '_func_')
+        .forEach(([key, handler]) => propogateHandlerDown(handler));
 };
 
 let doHandler = handler => {
@@ -61,18 +51,16 @@ let doHandler = handler => {
 let watchAll = (origin, handlers, accumulatedHandlers) => {
     Object.entries(handlers)
         .filter(([key]) => key !== '_func_')
-        .forEach(([key, handlerValue]) => {
-            let nextHandler = handlers[key];
+        .forEach(([key, handler]) => {
             let nextAccumulatedHandlers = accumulatedHandlers.concat(handlers);
             let watchKey = `_${key}_`;
             origin[watchKey] = origin[key];
-            watch(origin, key, () => {
-                let got = origin[watchKey];
-                return proxyGet(got, nextHandler, nextAccumulatedHandlers);
-            }, newValue => {
-                origin[watchKey] = newValue;
-                proxySet(key, nextHandler, nextAccumulatedHandlers)
-            });
+            watch(origin, key,
+                () => getHandler(origin[watchKey], handler, nextAccumulatedHandlers),
+                newValue => {
+                    origin[watchKey] = newValue;
+                    setHandler(key, handler, nextAccumulatedHandlers)
+                });
         });
 };
 
@@ -85,5 +73,3 @@ let watch = (obj, key, getHandler, setHandler) => {
 };
 
 module.exports = {createSource, createSourceWithHandlers};
-
-// todo remove clogs, 1 line lambdas, move out watchAll
