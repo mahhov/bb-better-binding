a880dfb24a2d2a1ea7140450e05ff98a6f503d26
m <mahhov1@gmail.com>
2018-05-03 18:49:19 -0700

    clean source a bit

--- src/source.js
+++ src/source.js
@@ -8,41 +8,35 @@ let ignore = [];
 
 let isBindIgnored = (obj, prop) => obj.__bindIgnore__ && obj.__bindIgnore__.includes(prop);
 
-let createProxy = (obj, handlers, accumulatedHandlers = []) => new Proxy(obj, {
+// todo make __bindAvoidCycles__ inherited and maybe avoid per binding instead per change
+let isIgnored = (obj, prop) => isBindIgnored(obj, prop) || (obj.__bindAvoidCycles__ && ignore.some(ignore => ignore.obj === obj && ignore.prop === prop));
+
+let handleSet = (obj, prop, handlers, accumulatedHandlers) => {
+    ignore.push({obj, prop});
+    accumulatedHandlers.forEach(doHandler);
+    handlers && propogateHandlerDown(handlers);
+    ignore.pop();
+};
+
+let createProxy = (obj, handlers = {}, accumulatedHandlers = []) => new Proxy(obj, {
     get: (target, prop) => {
         let got = Reflect.get(target, prop);
-        return typeof got === 'object' && got !== null && !isBindIgnored(obj, prop) ? createProxy(got, handlers && handlers[prop], accumulatedHandlers.concat(handlers)) : got;
+        return typeof got === 'object' && got && !isBindIgnored(obj, prop) ? createProxy(got, handlers[prop], accumulatedHandlers.concat(handlers)) : got;
     },
     set: (target, prop, value) => {
         Reflect.set(target, prop, value);
-
-        if (isBindIgnored(obj, prop))
-            return true;
-
-        // todo make __bindAvoidCycles__ inherited and maybe avoid per binding instead per change
-        if (obj.__bindAvoidCycles__ && ignore.some(ignore => ignore.obj === obj && ignore.prop === prop))
-            return true;
-
-        ignore.push({prop, obj});
-        accumulatedHandlers.forEach(doHandler);
-        doHandler(handlers);
-        handlers && propogateHandlerDown(handlers[prop]);
-        ignore.pop();
-
+        !isIgnored(obj, prop) && handleSet(obj, prop, handlers[prop], accumulatedHandlers.concat(handlers)); // todo wrap handlers and accumulatedHandlers in class with popProp method
         return true;
     }
 });
 
 let propogateHandlerDown = handlers => {
     doHandler(handlers);
-    for (key in handlers)
-        if (key !== '_func_')
-            propogateHandlerDown(handlers[key]);
+    Object.entries(handlers)
+        .filter(([key]) => key !== '_func_')
+        .forEach(([, handler]) => propogateHandlerDown(handler));
 };
 
-let doHandler = handler => {
-    if (handler && typeof handler._func_ === 'function')
-        handler._func_();
-};
+let doHandler = handler => typeof handler._func_ === 'function' && handler._func_();
 
 module.exports = {createSource};
