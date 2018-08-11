2177e9e76cc863f39b493e6d182fc563342f9f4c
mahhov <mahhov1@gmail.com>
2018-08-11 11:01:06 -0700

    allow optional manual triggering of source comparison

--- src/booter.js
+++ src/booter.js
@@ -10,11 +10,20 @@ class Booter {
         this.blocks[blockName] = block;
     }
 
-    boot(root, debug) {
+    boot(root, debug, manual) {
         let artifacts = new HtmlBinder(root, this.blocks).getArtifacts();
         debug && Object.assign(debug, artifacts);
-        setInterval(() => source.invokeAllHandlers(), 1);
+        if (!manual)
+            this.loop();
         return artifacts.source;
+    }
+
+    tick() {
+        source.invokeAllHandlers()
+    }
+
+    loop(interval = 1) {
+        setInterval(() => source.invokeAllHandlers(), interval);
     }
 }
 
